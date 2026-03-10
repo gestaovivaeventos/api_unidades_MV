41bd6979a33c0bfcfcb549a2420a18e53ef1625f
gestaovivaeventos <gestao@vivaeventos.com.br>
2026-03-10 10:25:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -46,52 +46,23 @@ def obter_dados(limit: int = 5000, offset: int = 0):
         with conn.cursor() as cursor:
             query = """
                 SELECT
-                    u.id,
-                    CASE
-                        WHEN uni.id = 31 THEN 'Itaperuna Muriae'
-                        ELSE uni.nm_unidade
-                    END AS nm_unidade,
-                    CASE
-                        WHEN uni_main.id = 31 THEN 'Itaperuna Muriae'
-                        ELSE uni_main.nm_unidade
-                    END AS nm_unidade_principal_desc,
-                    u.nome,
-                    u.username,
-                    u.enabled,
-                    uni.id AS id_unidade,
-                    uv.prioritaria AS unidade_principal,
-                    gr.name AS "nm_grupo/cargo",
-                    u.datacriacao AS dt_criacao,
-                    u.last_login AS ultimo_acesso
-                FROM
-                    tb_usuario u
-                    LEFT JOIN tb_unidade_vinculada uv ON uv.id_usuario = u.id
-                    LEFT JOIN tb_unidade uni ON uni.id = uv.id_unidade
-                    LEFT JOIN tb_grupo gr ON gr.id = uv.id_grupo
-                    JOIN tb_grupo_permissao per ON per.id_grupo = gr.id
-                    LEFT JOIN tb_setor s ON s.id = gr.id_setor
-                    LEFT JOIN tb_unidade_vinculada uv_main ON uv_main.id_usuario = u.id
-                    AND uv_main.prioritaria IS TRUE
-                    LEFT JOIN tb_unidade uni_main ON uni_main.id = uv_main.id_unidade
-                WHERE
-                    -- CORREÇÃO ABAIXO: Uso de %% para escapar a porcentagem
-                    gr.name NOT LIKE '%%Integrantes%%'
-                    AND gr.name NOT LIKE '%%Comissão%%'
-                    AND u.cpf IS NULL
-                    AND uni.id NOT IN (8,7,43,49,69,75,50,79,51,61,73,47,48,37)
-                    AND uni.fl_ativa
-                GROUP BY
-                    u.username,
-                    uni.nm_unidade,
-                    u.id,
-                    uni.id,
-                    s.setor,
-                    gr.name,
-                    uv.prioritaria,
-                    uni_main.id,
-                    uni_main.nm_unidade
-                ORDER BY
-                    u.id
+	id AS id_unidade,
+	nm_unidade,
+	fl_ativa,
+	CASE situacao
+		WHEN '1' THEN 'Comum'
+		WHEN '2' THEN 'Em renovação'
+		WHEN '3' THEN 'Em encerramento'
+		WHEN '4' THEN 'Implantação'
+		WHEN '5' THEN 'Operação'
+		ELSE 'Não informado'
+	END AS situacao_descricao,
+	dt_inauguracao,
+	dt_encerramento
+FROM
+	tb_unidade
+
+	where categoria = '2'
                 LIMIT %s OFFSET %s
             """
             cursor.execute(query, (limit, offset))
